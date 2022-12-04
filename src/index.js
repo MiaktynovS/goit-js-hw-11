e2e9586c33fd8ea093d0143f79c5ddd77442e5b8
MiaktynovS <ousm9595@gmail.com>
2022-12-04 13:00:31 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
-const axios = require('axios').default;
+// const axios = require('axios').default;
