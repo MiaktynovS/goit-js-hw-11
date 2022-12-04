2c0adad000125d1dfdd47ef27c245d288002f9e2
MiaktynovS <ousm9595@gmail.com>
2022-12-04 12:50:32 +0200

    Install libraries

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/index.scss'
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const axios = require('axios').default;
